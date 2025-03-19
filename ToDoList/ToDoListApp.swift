e8481820ca9881f0b301b0f99f3b96b73b1bace0
Yan <65673794+0-Luminous@users.noreply.github.com>
2025-03-19 14:41:49 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Yan on 19/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Yan on 19/3/25.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
